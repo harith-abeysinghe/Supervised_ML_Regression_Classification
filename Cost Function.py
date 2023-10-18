88786a57e67a47f292baf13f365a552de4084ebd
harith-abeysinghe <e19003@eng.pdn.ac.lk>
2023-10-18 15:48:39 +0530

    Update Cost Function.py

--- Cost Function.py
+++ Cost Function.py
@@ -1,10 +1,11 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from lab_utils_uni import plt_intuition, plt_stationary, plt_update_onclick, soup_bowl
+
 plt.style.use('./deeplearning.mplstyle')
 
-x_train = np.array([1.0, 2.0])           #(size in 1000 square feet)
-y_train = np.array([300.0, 500.0])           #(price in 1000s of dollars)
+x_train = np.array([1.0, 2.0])  # (size in 1000 square feet)
+y_train = np.array([300.0, 500.0])  # (price in 1000s of dollars)
 
 
 def compute_cost(x, y, w, b):
@@ -32,12 +33,13 @@ def compute_cost(x, y, w, b):
 
     return total_cost
 
-plt_intuition(x_train,y_train)
+
+plt_intuition(x_train, y_train)
 plt.show()
 
 # Using Larger Dataset
 x_train = np.array([1.0, 1.7, 2.0, 2.5, 3.0, 3.2])
-y_train = np.array([250, 300, 480,  430,   630, 730])
+y_train = np.array([250, 300, 480, 430, 630, 730])
 
 plt.close('all')
 fig, ax, dyn_items = plt_stationary(x_train, y_train)
