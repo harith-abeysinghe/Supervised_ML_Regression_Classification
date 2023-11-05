73822fe89ec29aa49e1d68d1061237d7f856fd2d
harith-abeysinghe <e19003@eng.pdn.ac.lk>
2023-11-05 13:08:35 +0530

    Add Multiple_Linear_Regression.py

---
+++ Multiple_Linear_Regression.py
@@ -0,0 +1,97 @@
+import copy, math
+import numpy as np
+import matplotlib.pyplot as plt
+
+plt.style.use('./deeplearning.mplstyle')
+np.set_printoptions(precision=2)
+
+X_train = np.array([[2104, 5, 1, 45], [1416, 3, 2, 40], [852, 2, 1, 35]])
+y_train = np.array([460, 232, 178])
+
+# data is stored in numpy array/matrix
+print(f"X Shape: {X_train.shape}, X Type:{type(X_train)})")
+print(X_train)
+print(f"y Shape: {y_train.shape}, y Type:{type(y_train)})")
+print(y_train)
+
+b_init = 785.1811367994083
+w_init = np.array([0.39133535, 18.75376741, -53.36032453, -26.42131618])
+print(f"w_init shape: {w_init.shape}, b_init type: {type(b_init)}")
+
+
+def predict(x, w, b):
+    """
+    single predict using linear regression
+    Args:
+      x (ndarray): Shape (n,) example with multiple features
+      w (ndarray): Shape (n,) model parameters
+      b (scalar):             model parameter
+
+    Returns:
+      p (scalar):  prediction
+    """
+    p = np.dot(x, w) + b
+    return p
+
+
+# get a row from our training data
+x_vec = X_train[0, :]
+print(f"x_vec shape {x_vec.shape}, x_vec value: {x_vec}")
+
+# make a prediction
+f_wb = predict(x_vec, w_init, b_init)
+print(f"f_wb shape {f_wb.shape}, prediction: {f_wb}")
+
+
+def compute_cost(X, y, w, b):
+    """
+    compute cost
+    Args:
+      X (ndarray (m,n)): Data, m examples with n features
+      y (ndarray (m,)) : target values
+      w (ndarray (n,)) : model parameters
+      b (scalar)       : model parameter
+
+    Returns:
+      cost (scalar): cost
+    """
+    m = X.shape[0]
+    cost = 0.0
+    for i in range(m):
+        f_wb_i = np.dot(X[i], w) + b  # (n,)(n,) = scalar (see np.dot)
+        cost = cost + (f_wb_i - y[i]) ** 2  # scalar
+    cost = cost / (2 * m)  # scalar
+    return cost
+
+
+# Compute and display cost using our pre-chosen optimal parameters.
+cost = compute_cost(X_train, y_train, w_init, b_init)
+print(f'Cost at optimal w : {cost}')
+
+
+def compute_gradient(X, y, w, b):
+    """
+    Computes the gradient for linear regression
+    Args:
+      X (ndarray (m,n)): Data, m examples with n features
+      y (ndarray (m,)) : target values
+      w (ndarray (n,)) : model parameters
+      b (scalar)       : model parameter
+
+    Returns:
+      dj_dw (ndarray (n,)): The gradient of the cost w.r.t. the parameters w.
+      dj_db (scalar):       The gradient of the cost w.r.t. the parameter b.
+    """
+    m, n = X.shape  # (number of examples, number of features)
+    dj_dw = np.zeros((n,))
+    dj_db = 0.
+
+    for i in range(m):
+        err = (np.dot(X[i], w) + b) - y[i]
+        for j in range(n):
+            dj_dw[j] = dj_dw[j] + err * X[i, j]
+        dj_db = dj_db + err
+    dj_dw = dj_dw / m
+    dj_db = dj_db / m
+
+    return dj_db, dj_dw
