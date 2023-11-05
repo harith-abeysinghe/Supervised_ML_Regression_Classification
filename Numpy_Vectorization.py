16823352f011700668d2408d6ac07f7adef99cfa
harith-abeysinghe <e19003@eng.pdn.ac.lk>
2023-11-05 12:58:02 +0530

    Update Numpy_Vectorization.py

--- Numpy_Vectorization.py
+++ Numpy_Vectorization.py
@@ -131,3 +131,91 @@ def my_dot(a, b):
     for i in range(a.shape[0]):
         x = x + a[i] * b[i]
     return x
+
+
+print()
+print("Using my_dot Function")
+# test 1-D
+a = np.array([1, 2, 3, 4])
+b = np.array([-1, 4, 3, 2])
+print(f"my_dot(a, b) = {my_dot(a, b)}")
+
+# test 1-D
+a = np.array([1, 2, 3, 4])
+b = np.array([-1, 4, 3, 2])
+c = np.dot(a, b)
+print(f"NumPy 1-D np.dot(a, b) = {c}, np.dot(a, b).shape = {c.shape} ")
+c = np.dot(b, a)
+print(f"NumPy 1-D np.dot(b, a) = {c}, np.dot(a, b).shape = {c.shape} ")
+
+print()
+print("User-Defined Function vs. Numpy Dot Product")
+np.random.seed(1)
+a = np.random.rand(10000000)  # very large arrays
+b = np.random.rand(10000000)
+
+tic = time.time()  # capture start time
+c = np.dot(a, b)
+toc = time.time()  # capture end time
+
+print(f"np.dot(a, b) =  {c:.4f}")
+print(f"Vectorized version duration: {1000*(toc-tic):.4f} ms ")
+
+tic = time.time()  # capture start time
+c = my_dot(a,b)
+toc = time.time()  # capture end time
+
+print(f"my_dot(a, b) =  {c:.4f}")
+print(f"loop version duration: {1000*(toc-tic):.4f} ms ")
+
+del a
+del b  #remove these big arrays from memory
+
+
+X = np.array([[1],[2],[3],[4]])
+w = np.array([2])
+c = np.dot(X[1], w)
+
+print()
+print(f"X[1] has shape {X[1].shape}")
+print(f"w has shape {w.shape}")
+print(f"c has shape {c.shape}")
+
+print()
+print("Matrix Creation")
+a = np.zeros((1, 5))
+print(f"a shape = {a.shape}, a = {a}")
+
+a = np.zeros((2, 1))
+print(f"a shape = {a.shape}, a = {a}")
+
+a = np.random.random_sample((1, 1))
+print(f"a shape = {a.shape}, a = {a}")
+
+# NumPy routines which allocate memory and fill with user specified values
+a = np.array([[5], [4], [3]]);   print(f" a shape = {a.shape}, np.array: a = {a}")
+a = np.array([[5],   # One can also
+              [4],   # separate values
+              [3]]); #into separate rows
+print(f" a shape = {a.shape}, np.array: a = {a}")
+
+print()
+print("Indexing")
+
+#vector 2-D slicing operations
+a = np.arange(20).reshape(-1, 10)
+print(f"a = \n{a}")
+
+#access 5 consecutive elements (start:stop:step)
+print("a[0, 2:7:1] = ", a[0, 2:7:1], ",  a[0, 2:7:1].shape =", a[0, 2:7:1].shape, "a 1-D array")
+
+#access 5 consecutive elements (start:stop:step) in two rows
+print("a[:, 2:7:1] = \n", a[:, 2:7:1], ",  a[:, 2:7:1].shape =", a[:, 2:7:1].shape, "a 2-D array")
+
+# access all elements
+print("a[:,:] = \n", a[:,:], ",  a[:,:].shape =", a[:,:].shape)
+
+# access all elements in one row (very common usage)
+print("a[1,:] = ", a[1,:], ",  a[1,:].shape =", a[1,:].shape, "a 1-D array")
+# same as
+print("a[1]   = ", a[1],   ",  a[1].shape   =", a[1].shape, "a 1-D array")
